b24c1bb53f96423141370ec6f92f48eb7464c251
munseng-flexiv <munseng.phoon@flexiv.com>
2024-11-19 16:06:47 +0800

    Change joint position control to NRT mode

--- flexiv_hardware/src/flexiv_hardware_interface.cpp
+++ flexiv_hardware/src/flexiv_hardware_interface.cpp
@@ -17,6 +17,13 @@
 #include "flexiv/rdk/robot.hpp"
 #include "flexiv_hardware/flexiv_hardware_interface.hpp"
 
+namespace {
+
+constexpr double kMaxJointVelocity = 2.0;
+constexpr double kMaxJointAcceleration = 3.0;
+
+}
+
 namespace flexiv_hardware {
 
 hardware_interface::CallbackReturn FlexivHardwareInterface::on_init(
@@ -274,6 +281,9 @@ hardware_interface::return_type FlexivHardwareInterface::write(
     std::vector<double> target_vel(robot_->info().DoF);
     std::vector<double> target_acc(robot_->info().DoF);
 
+    std::vector<double> max_vel(robot_->info().DoF, kMaxJointVelocity);
+    std::vector<double> max_acc(robot_->info().DoF, kMaxJointAcceleration);
+
     bool isNanPos = false;
     bool isNanVel = false;
     bool isNanEff = false;
@@ -289,15 +299,15 @@ hardware_interface::return_type FlexivHardwareInterface::write(
         }
     }
 
-    if (position_controller_running_ && robot_->mode() == flexiv::rdk::Mode::RT_JOINT_POSITION
+    if (position_controller_running_ && robot_->mode() == flexiv::rdk::Mode::NRT_JOINT_POSITION
         && !isNanPos) {
         target_pos = hw_commands_joint_positions_;
-        robot_->StreamJointPosition(target_pos, target_vel, target_acc);
+        robot_->SendJointPosition(target_pos, target_vel, target_acc, max_vel, max_acc);
     } else if (velocity_controller_running_
-               && robot_->mode() == flexiv::rdk::Mode::RT_JOINT_POSITION && !isNanVel) {
+               && robot_->mode() == flexiv::rdk::Mode::NRT_JOINT_POSITION && !isNanVel) {
         target_pos = hw_commands_joint_positions_;
         target_vel = hw_commands_joint_velocities_;
-        robot_->StreamJointPosition(target_pos, target_vel, target_acc);
+        robot_->SendJointPosition(target_pos, target_vel, target_acc, max_vel, max_acc);
     } else if (torque_controller_running_ && robot_->mode() == flexiv::rdk::Mode::RT_JOINT_TORQUE
                && !isNanEff) {
         std::vector<double> target_torque(robot_->info().DoF);
@@ -420,7 +430,7 @@ hardware_interface::return_type FlexivHardwareInterface::perform_command_mode_sw
             std::numeric_limits<double>::quiet_NaN());
 
         // Set to joint position mode
-        robot_->SwitchMode(flexiv::rdk::Mode::RT_JOINT_POSITION);
+        robot_->SwitchMode(flexiv::rdk::Mode::NRT_JOINT_POSITION);
 
         position_controller_running_ = true;
     } else if (start_modes_.size() != 0
@@ -435,7 +445,7 @@ hardware_interface::return_type FlexivHardwareInterface::perform_command_mode_sw
             std::numeric_limits<double>::quiet_NaN());
 
         // Set to joint position mode
-        robot_->SwitchMode(flexiv::rdk::Mode::RT_JOINT_POSITION);
+        robot_->SwitchMode(flexiv::rdk::Mode::NRT_JOINT_POSITION);
 
         velocity_controller_running_ = true;
     } else if (start_modes_.size() != 0
